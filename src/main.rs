84cd973f7c7166b8b8e5afa070f148d24194e3ce
Robert Straw <drbawb@fatalsyntax.com>
2014-10-24 19:17:27 -0500

    Update to handle new actions

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,31 @@ extern crate serialize;
 use p150::P150Cpu;
 
 use serialize::json;
+use serialize::hex::FromHex;
 use std::collections::HashMap;
 use std::io::net::ip::Ipv4Addr;
 use std::sync::{Arc,RWLock};
-use nickel::{Nickel, Request, Response};
+use nickel::{JsonBody, Nickel, Request, Response};
 use nickel::{Middleware, HttpRouter, StaticFilesHandler};
 use nickel::{MiddlewareResult, NickelError};
 
 mod p150;
 
+static MAX_CYCLES: i32 = 100000; // maximum number of cycles CPU can execute to prevent DoS attacks.
+
+/// Injects a mutex-protected CpuServ into request map when invoked
 struct CpuMw { db: Arc<RWLock<CpuServ>> }
+
+/// A database of multiple uniquely identified P150 CPU emulators.
+///
+/// Each emulator maintains its own state and can be recalled
+/// using the integer identification.
 struct CpuServ {
 	next_cpu: i32,
 	database: HashMap<i32, P150Cpu>,
 }
 
 impl Middleware for CpuMw {
-	/// Injects the protected CPU map into the request
 	fn invoke(&self, req: &mut Request, _res: &mut Response) -> MiddlewareResult {
 		req.map.insert(self.db.clone());
 		Ok(nickel::Continue)
@@ -50,6 +58,15 @@ fn main() {
 		response.render("./assets/views/index.html", &page);
 	}
 
+	fn about_show(_request: &Request, response: &mut Response) {
+		let mut page = HashMap::new();
+		page.insert("title", "P150 Emulator");
+
+		response.render("./assets/views/about.html", &page);
+	}
+
+	/// POST /cpu/new: creates a new CPU and adds it to the CpuServ with the next
+	/// available ID number.
 	fn cpu_new(req: &Request, response: &mut Response) {
 		match req.map.find::<Arc<RWLock<CpuServ>>>() {
 			Some(cpuserv) => {
@@ -65,29 +82,82 @@ fn main() {
 		}
 	}
 
+	/// POST /cpu/:id/load
+	/// Overwrites the CPUs system memory with response data.
+	/// The current machine state is then dumped into the response as JSON.
 	fn cpu_load(req: &Request, response: &mut Response) {
 		let id = from_str::<i32>(req.param("id")).unwrap();
+		let mem_js = req.json_as::<Vec<String>>().unwrap();
+		let mem_native: Vec<u8> = mem_js.iter().map(|hex| {
+			hex.as_slice().from_hex().unwrap()[0]
+		}).collect();
+
+		// pair odd u8s with even u8s to produce u16 ops.
+		let even_mn = mem_native.iter().enumerate().filter(|&(idx, _)| { idx % 2 == 0 });
+		let odd_mn  = mem_native.iter().enumerate().filter(|&(idx, _)| { idx % 2 != 0 });
+		let ops: Vec<u16> = even_mn.zip(odd_mn).map(|((_, &msb), (_, &lsb))| {
+			(msb as u16 << 8) | (lsb as u16)
+		}).collect();
+
 		match req.map.find::<Arc<RWLock<CpuServ>>>() {
 			Some(cpuserv) => {
 				let mut w_serv = cpuserv.write();
 				let cpu        = w_serv.database.find_mut(&id).unwrap();
 
-				cpu.init_mem([0x1001, 0xB000]); // TODO: load from req.
+				cpu.init_mem(ops.as_slice()); // TODO: load from req.
 				response.send(format!("{}", cpu.js_dump()));
 			},
 			None => { response.send("did not find cpu serv"); }
 		}
 	}
 
+	/// POST /cpu/:id/dump
+	/// Dumps the CPUs current state to a JSON format.
+	/// This request does not modify the CPU state in any way.
+	fn cpu_dump(req: &Request, response: &mut Response) {
+		let id = from_str::<i32>(req.param("id")).unwrap();
+		match req.map.find::<Arc<RWLock<CpuServ>>>() {
+			Some(cpuserv) => {
+				let r_serv = cpuserv.read();
+				let cpu        = r_serv.database.find(&id).unwrap();
+
+				response.send(format!("{}", cpu.js_dump()));
+			},
+			None => { response.send("did not find cpu serv"); }
+		}
+	}
+
+
+	/// POST /cpu/:id/tick
+	/// Resumes execution from the next instruction; running the machine to completion.
+	/// The resultant state is then dumped to the console.
+	fn cpu_tick(req: &Request, response: &mut Response) {
+		let id = from_str::<i32>(req.param("id")).unwrap();
+		match req.map.find::<Arc<RWLock<CpuServ>>>() {
+			Some(cpuserv) => {
+				let mut w_serv = cpuserv.write();
+				let cpu        = w_serv.database.find_mut(&id).unwrap();
+				let _          = cpu.tick();
+
+				response.send("");
+			},
+			None => { response.send("did not find cpu serv"); }
+		}
+	}
+
+
+	/// POST /cpu/:id/run
+	/// Resumes execution from the next instruction; running the machine to completion.
+	/// The resultant state is then dumped to the console.
 	fn cpu_run(req: &Request, response: &mut Response) {
 		let id = from_str::<i32>(req.param("id")).unwrap();
 		match req.map.find::<Arc<RWLock<CpuServ>>>() {
 			Some(cpuserv) => {
-				let mut cycles = 0i16;
+				let mut cycles = 0i32;
 				let mut w_serv = cpuserv.write();
 				let cpu    = w_serv.database.find_mut(&id).unwrap();
 				loop {
-					if cycles == 10000 { println!("CPU executed > 10000 cycles."); break; }
+					if cycles >= MAX_CYCLES { println!("CPU executed > {} cycles.", MAX_CYCLES); break; }
 					cycles += 1;
 
 					match cpu.tick() {
@@ -96,19 +166,26 @@ fn main() {
 					}
 				}
 
-				response.send(format!("{}", cpu.js_dump()));
+				response.send("");
 			},
 			None => { response.send("did not find cpu serv"); }
 		}
 	}
 
 	router.get("/", index_show);
+	router.get("/about", about_show);
+
 	router.post("/cpu/new", cpu_new);
-	router.post("/cpu/:id/load", cpu_load);
+	router.post("/cpu/:id/tick", cpu_tick);
 	router.post("/cpu/:id/run", cpu_run);
+	router.post("/cpu/:id/load", cpu_load);
+	router.get("/cpu/:id/dump", cpu_dump);
+	// TODO: /cpu/:id/tick, /cpu/:id/reset
+	// TODO: /cpu/:id/save ???
 
 	server.utilize(CpuMw { db: Arc::new(RWLock::new(CpuServ::new())) });
 	server.utilize(StaticFilesHandler::new("./public"));
+	server.utilize(Nickel::json_body_parser());
 	server.utilize(router);
 	
 	server.listen(Ipv4Addr(0,0,0,0), 3200);
